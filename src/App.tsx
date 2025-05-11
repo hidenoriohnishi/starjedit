fbf939f20dc12e02fe7d3e13027aab52a2f135dd
Hidenori Ohnishi <8862042+hidenoriohnishi@users.noreply.github.com>
2025-05-11 16:16:56 +0900

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
 import StarWarsEditor from './components/StarWarsEditor'
 import React from 'react'
+import { useScrollAnimation } from './hooks/useScrollAnimation'
+import starLogo from './assets/star-jedit-logo.svg'
 
 function App() {
+  const scrollProgress = useScrollAnimation();
+
   return (
     <div className="App">
-      <StarWarsEditor />
+      <div 
+        className="editor-container"
+        style={{
+          transform: `translateY(${(1 - scrollProgress) * 100}vh)`
+        }}
+      >
+        <StarWarsEditor />
+      </div>
+      <div 
+        className="logo-container"
+        style={{
+          opacity: 1 - scrollProgress
+        }}
+      >
+        <img src={starLogo} alt="Star JEdit Logo" className="main-logo" />
+      </div>
     </div>
   )
 }
